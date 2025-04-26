82a417b94b31d087157c06df59e2acdd0c1632cf
ijlalahmad2002 <ijlalahmad2002@gmail.com>
2025-04-26 21:43:55 +0500

    MRI_Project

---
+++ Preprocessing_Steps/02-Slice_Timing/Code_Files/Slice_Time.m
@@ -0,0 +1,9 @@
+% List of open inputs
+nrun = X; % enter the number of runs here
+jobfile = {'D:\University\DS_Project\Preprocessing\DS_Tasks\Preprocessing_Steps\02-Slice_Timing\Code_Files\Slice_Time_job.m'};
+jobs = repmat(jobfile, 1, nrun);
+inputs = cell(0, nrun);
+for crun = 1:nrun
+end
+spm('defaults', 'FMRI');
+spm_jobman('run', jobs, inputs{:});
---
+++ Preprocessing_Steps/03-Coregestration/Code_Files/Coregres_job.m
@@ -0,0 +1,16 @@
+%-----------------------------------------------------------------------
+% Job saved on 23-Apr-2025 12:08:50 by cfg_util (rev $Rev: 7345 $)
+% spm SPM - SPM12 (7771)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.coreg.estwrite.ref = {'C:\spm12-main\ds000003_R2.0.2\sub-07\func\meansub-07_task-rhymejudgment_bold.nii,1'};
+matlabbatch{1}.spm.spatial.coreg.estwrite.source = {'C:\spm12-main\ds000003_R2.0.2\sub-07\anat\sub-07_T1w.nii,1'};
+matlabbatch{1}.spm.spatial.coreg.estwrite.other = {''};
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = 'r';
